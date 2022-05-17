30401bd1a43c104e0597a825c40bb165672b49d9
lucas <lteles350@gmail.com>
2022-05-17 10:19:36 -0300

    Terceiro dia do Bootcamp desafio de 100 dias em python.

--- Day002/Project_Day002/the_tip_calculator.py
+++ Day002/Project_Day002/the_tip_calculator.py
@@ -21,3 +21,4 @@ tip_percentage = int(input('How much tip woud you like to give? 10, 12, or 15? '
 amount_of_people = int(input('How many people to split the bill? '))
 pay_per_pearson = round(total_bill * (1 + (tip_percentage / 100)) / amount_of_people, 2)
 print(f'Each pearson should pay: ${pay_per_pearson}')
+
